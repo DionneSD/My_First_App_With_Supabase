6b7cc719d337e3acf55439cbffce34c2b42be8c2
DionneSD <111356485+DionneSD@users.noreply.github.com>
2023-08-24 21:05:09 +0100

    css fixed

--- src/components/ActivityCard.js
+++ src/components/ActivityCard.js
@@ -1,37 +1,38 @@
-import { Link } from 'react-router-dom';
-import supabase from '../supabaseClient';
+import { Link } from "react-router-dom";
+import supabase from "../supabaseClient";
 
-function ActivityCard({ activity, onDelete }){
+function ActivityCard({ activity, onDelete }) {
+  const handleDelete = async () => {
+    const { data, error } = await supabase
+      .from("activities")
+      .delete()
+      .eq("id", activity.id)
+      .select();
 
-    const handleDelete = async () => {
-        const { data, error } = await supabase
-            .from('activities')
-            .delete()
-            .eq('id', activity.id)
-            .select();
-
-        if (error) {
-            console.log(error);
-        }
-        if (data) {
-            console.log(data);
-            onDelete(activity.id);
-        }
+    if (error) {
+      console.log(error);
     }
+    if (data) {
+      console.log(data);
+      onDelete(activity.id);
+    }
+  };
 
-    return (
-        <div className='activity-card'>
-            <h3>{activity.activity}</h3>
-            <p>{activity.instruction}</p>
-            <div className='minutes'>{activity.minutes}</div>
-            <div className='buttons'>
-                <Link to={'/' + activity.id}>
-                    <i className='material-icons'>edit</i>
-                </Link>
-                <i className='material-icons' onClick={handleDelete}>delete</i>
-            </div>
-        </div>
-    )
+  return (
+    <div className="activity-card">
+      <h3>{activity.activity}</h3>
+      <p>{activity.instruction}</p>
+      <div className="minutes">{activity.minutes}</div>
+      <div className="i-buttons">
+        <Link to={"/" + activity.id}>
+          <i className="material-icons">edit</i>
+        </Link>
+        <i className="material-icons" onClick={handleDelete}>
+          delete
+        </i>
+      </div>
+    </div>
+  );
 }
 
 export default ActivityCard;
